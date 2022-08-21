ceef91a7dafc2477d38d637e7643a9271110aa3e
HelloWorldItsMeYourProgrammer <92838200+HelloWorldItsMeYourProgrammer@users.noreply.github.com>
2022-08-22 00:16:28 +0300

    Eigth Task

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,15 @@ subtraction(a, b) - priima du skaičius ir grąžina jų skirtumą.
 multiplication(a, b) - priima du skaičius ir grąžina jų daugybos rezultatą;
 division(a, b) - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+function Calculator (a, b) {
+    this.a = a;
+    this.b = b;
+    this.aritmetika = function () {
+        alert("suma: " + (a+b) + " atimtis: " + (a-b) + " daugyba: " + (a*b) + " dalyba: " + (a/b));
+    }
+}
+
+const myCalculator = new Calculator (3, 18);
+
+myCalculator.aritmetika();
